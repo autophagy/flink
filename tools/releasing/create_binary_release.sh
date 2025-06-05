a27017d0ef03ea679d9dd101571ef2fc730e3b3e
Mika Naylor <mnaylor@confluent.io>
2025-06-05 17:12:32 -0400

    Remove uv pip install from groups
    Will be fixed in: https://github.com/astral-sh/uv/pull/13742

--- tools/releasing/create_binary_release.sh
+++ tools/releasing/create_binary_release.sh
@@ -88,7 +88,7 @@ make_python_release() {
   # use lint-python.sh script to create a python environment.
   dev/lint-python.sh -s basic
   source dev/.uv/bin/activate
-  uv pip install --group development
+  pip install --group development
 
   # build apache-flink-libraries sdist
   pushd apache-flink-libraries
