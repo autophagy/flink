f4bf4027a93701b5a3b5a0d0066e909ad415e768
Mika Naylor <mnaylor@confluent.io>
2026-05-11 15:06:43 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! py313

--- flink-python/pyflink/fn_execution/datastream/embedded/state_impl.py
+++ flink-python/pyflink/fn_execution/datastream/embedded/state_impl.py
@@ -223,7 +223,7 @@ class MapStateImpl(KeyedStateImpl, InternalMapState):
         entries = self._state.entries()
         if entries:
             for entry in entries:
-                raise Exception(str(dir(entry)))
+                raise Exception(f"{str(dir(entry))} // {entry} // {type(entry)}")
                 yield (self._k_converter.to_internal(entry.getKey()),
                        self._v_converter.to_internal(entry.getValue()))
 
