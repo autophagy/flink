00ca76e66dad4cb9c6ad2ec03a0f7768ca0197d8
Mika Naylor <mnaylor@confluent.io>
2026-06-01 12:01:11 +0200

    TTL

--- flink-table/flink-table-test-utils/src/main/java/org/apache/flink/table/runtime/functions/StateConverter.java
+++ flink-table/flink-table-test-utils/src/main/java/org/apache/flink/table/runtime/functions/StateConverter.java
@@ -22,7 +22,7 @@ import org.apache.flink.annotation.Internal;
 
 /**
  * Converter between external state representations (ListView, MapView, Row & Pojo state) and
- * internal storage formats (ArrayData, MapData, & RowData).
+ * internal storage formats.
  */
 @Internal
 interface StateConverter {
@@ -33,6 +33,12 @@ interface StateConverter {
     /** Converts an internal storage format to external state object. */
     Object toExternal(Object internal);
 
-    /** Create new internal state instance. */
+    /** Creates a new empty internal state instance. */
     Object createNewInternalState();
+
+    /**
+     * Evicts expired elements/entries from a TTL-aware internal state object. Only DataView
+     * converters override this; value state eviction is handled by the state manager directly.
+     */
+    default void evictExpired(Object internalState, long now, long ttlMillis) {}
 }
